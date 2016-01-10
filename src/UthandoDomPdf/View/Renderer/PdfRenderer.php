95f02110292ac32eaa1282231f8452a35aebb037
shaunfreeman <shaun@shaunfreeman.co.uk>
2016-01-10 22:29:01 +0000

    fixed blank page when printing

--- src/UthandoDomPdf/View/Renderer/PdfRenderer.php
+++ src/UthandoDomPdf/View/Renderer/PdfRenderer.php
@@ -24,6 +24,65 @@ use DOMPDF;
  */
 class PdfRenderer implements Renderer
 {
+    static $PAPER_SIZES = array(
+        "4a0" => array(0,0,4767,6740),
+        "2a0" => array(0,0,3370,4767),
+        "a0" => array(0,0,2383,3370),
+        "a1" => array(0,0,1683,2383),
+        "a2" => array(0,0,1190,1683),
+        "a3" => array(0,0,841,1190),
+        "a4" => array(0,0,595,841),
+        "a5" => array(0,0,419,595),
+        "a6" => array(0,0,297,419),
+        "a7" => array(0,0,209,297),
+        "a8" => array(0,0,147,209),
+        "a9" => array(0,0,104,147),
+        "a10" => array(0,0,73,104),
+        "b0" => array(0,0,2834,4008),
+        "b1" => array(0,0,2004,2834),
+        "b2" => array(0,0,1417,2004),
+        "b3" => array(0,0,1000,1417),
+        "b4" => array(0,0,708,1000),
+        "b5" => array(0,0,498,708),
+        "b6" => array(0,0,354,498),
+        "b7" => array(0,0,249,354),
+        "b8" => array(0,0,175,249),
+        "b9" => array(0,0,124,175),
+        "b10" => array(0,0,87,124),
+        "c0" => array(0,0,2599,3676),
+        "c1" => array(0,0,1836,2599),
+        "c2" => array(0,0,1298,1836),
+        "c3" => array(0,0,918,1298),
+        "c4" => array(0,0,649,918),
+        "c5" => array(0,0,459,649),
+        "c6" => array(0,0,323,459),
+        "c7" => array(0,0,229,323),
+        "c8" => array(0,0,161,229),
+        "c9" => array(0,0,113,161),
+        "c10" => array(0,0,79,113),
+        "ra0" => array(0,0,2437,3458),
+        "ra1" => array(0,0,1729,2437),
+        "ra2" => array(0,0,1218,1729),
+        "ra3" => array(0,0,864,1218),
+        "ra4" => array(0,0,609,864),
+        "sra0" => array(0,0,2551,3628),
+        "sra1" => array(0,0,1814,2551),
+        "sra2" => array(0,0,1275,1814),
+        "sra3" => array(0,0,907,1275),
+        "sra4" => array(0,0,637,907),
+        "letter" => array(0,0,612.00,792.00),
+        "legal" => array(0,0,612.00,1008.00),
+        "ledger" => array(0,0,1224.00, 792.00),
+        "tabloid" => array(0,0,792.00, 1224.00),
+        "executive" => array(0,0,521.86,756.00),
+        "folio" => array(0,0,612.00,936.00),
+        "commercial #10 envelope" => array(0,0,684,297),
+        "catalog #10 1/2 envelope" => array(0,0,648,864),
+        "8.5x11" => array(0,0,612.00,792.00),
+        "8.5x14" => array(0,0,612.00,1008.0),
+        "11x17"  => array(0,0,792.00, 1224.00),
+    );
+
     /**
      * @var DOMPDF
      */
@@ -85,7 +144,7 @@ class PdfRenderer implements Renderer
     public function render($nameOrModel, $values = null)
     {
         $pdfOptions         = $nameOrModel->getPdfOptions();
-        $paperSize          = $pdfOptions->getPaperSize();
+        $paperSize          = self::$PAPER_SIZES[strtolower($pdfOptions->getPaperSize())];
         $paperOrientation   = $pdfOptions->getPaperOrientation();
         $basePath           = $pdfOptions->getBasePath();
         
